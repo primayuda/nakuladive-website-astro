b38d44fc3010865b4b0464d714b19412fbb6c844
Aprita Primayuda <aprita.primayuda@gmail.com>
2024-06-02 21:28:19 +0700

    modifying config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
 import astroRemark from '@astrojs/markdown-remark';
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig(
-	/** @type {import('astro').AstroUserConfig} */
-	{
-		markdown: {
-			render: [astroRemark, {
-				remarkPlugins: [],
-				rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-					behavior: 'wrap'
-				}]],
-				shikiConfig: {
-					theme: 'poimandres',
-					langs: [],
-					wrap: false
-				}
-			}]
-		},
-		site: 'http://localhost:3000/',
-		integrations: [sitemap()]
-	});
+export default defineConfig( /** @type {import('astro').AstroUserConfig} */
+{
+  markdown: {
+    render: [astroRemark, {
+      remarkPlugins: [],
+      rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+        behavior: 'wrap'
+      }]],
+      shikiConfig: {
+        theme: 'poimandres',
+        langs: [],
+        wrap: false
+      }
+    }]
+  },
+  site: 'http://localhost:3000/',
+  integrations: [sitemap(), react(), tailwind()]
+});
